ec749d788cfb694c307662f1f148235595e06d09
Yang Zhong <yzhong@yugabyte.com>
2023-04-26 20:16:54 +0000

    formatting

--- src/yb/yql/pgwrapper/pg_single_tserver-test.cc
+++ src/yb/yql/pgwrapper/pg_single_tserver-test.cc
@@ -10,7 +10,7 @@
 // or implied.  See the License for the specific language governing permissions and limitations
 // under the License.
 //
-//
+
 #include "yb/common/ybc_util.h"
 
 #include "yb/tablet/tablet.h"
